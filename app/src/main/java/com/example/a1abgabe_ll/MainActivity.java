4ae6f5ff861fcbe7457d46341fe432eca62d8d58
Laura_LL <lauralik@edu.aau.at>
2022-03-10 18:25:53 +0100

    add login

--- app/src/main/java/com/example/a1abgabe_ll/MainActivity.java
+++ app/src/main/java/com/example/a1abgabe_ll/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 MatrikelNMR.setText("Deine Matrikelnummer: ");
             }
         });
+        System.out.println("New");
         //Prim-Code
         /*
         public class prim {
